637928f860cbbd697cad544139a9a156d77f8460
alpha-tango-kilo <git@heyatk.com>
2022-09-03 22:24:12 +0100

    Specify directory to read files from

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+use std::{env, process};
 use code_visualizer::renderer;
 
 fn main() {
-    let paths = renderer::get_unicode_files_in_dir("./input/");
+    let mut code_dir = env::args()
+        .nth(1)
+        .unwrap_or_else(|| {
+            println!("USAGE: code-visualizer <directory>");
+            process::exit(1);
+        });
+    if !code_dir.ends_with('/') {
+        code_dir.push('/');
+    }
+    let paths = renderer::get_unicode_files_in_dir(&code_dir);
 
     // render files to image, and store in ./output.png
     renderer::render(&paths, 100, 16.0 / 9.0, true, true)
