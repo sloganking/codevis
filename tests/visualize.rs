a5562aa3f66fbba7a1b55ce08287e6108e32f979
Sebastian Thiel <sebastian.thiel@icloud.com>
2022-09-06 15:45:26 +0800

    add test to show the problem

--- tests/visualize.rs
+++ tests/visualize.rs
@@ -1,9 +1,10 @@
+use bstr::ByteSlice;
 use code_visualizer::render;
 use std::path::Path;
 use std::sync::atomic::AtomicBool;
 
 #[test]
-fn renders_self() {
+fn various_renders() {
     let (paths, ignored) = code_visualizer::unicode_content(
         Path::new("./src/"),
         &[],
@@ -57,4 +58,50 @@ fn renders_self() {
         opts,
     )
     .unwrap();
+}
+
+#[test]
+#[ignore]
+fn multi_threading_produces_same_result_as_single_threaded_mode() {
+    let (paths, ignored) = code_visualizer::unicode_content(
+        Path::new("./src/"),
+        &[],
+        prodash::progress::Discard,
+        &AtomicBool::default(),
+    )
+    .unwrap();
+    assert_eq!(ignored, 0, "no ignore pattern configured");
+
+    let theme = "Solarized (dark)";
+    let mut opts = render::Options {
+        column_width: 100,
+        line_height: 1,
+        target_aspect_ratio: 0.0,
+        fg_color: code_visualizer::render::FgColor::Style,
+        bg_color: code_visualizer::render::BgColor::Style,
+        threads: 1,
+        theme,
+        force_full_columns: false,
+        ignore_files_without_syntax: true,
+    };
+    let expected = code_visualizer::render(
+        paths.clone(),
+        prodash::progress::Discard,
+        &AtomicBool::default(),
+        opts,
+    )
+    .unwrap();
+
+    opts.threads = 2;
+    let actual = code_visualizer::render(
+        paths.clone(),
+        prodash::progress::Discard,
+        &AtomicBool::default(),
+        opts,
+    )
+    .unwrap();
+    assert!(
+        actual.as_bytes() == expected.as_bytes(),
+        "multi-threaded version should be pixel-perfect"
+    );
 }
